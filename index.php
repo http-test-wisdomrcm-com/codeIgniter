950aa71f37caac8277260104101edc2376b7c051
geje2005 <geje2005@gmail.com>
2023-09-29 07:41:50 +0530

    first commit

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Congratulations GEORGE wisdomRCM!</h1>
+        <h1>Congratulations GEORGE JEBARAJ wisdomRCM!</h1>
 
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
